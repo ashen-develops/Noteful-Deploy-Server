c95c8231c7edf0ac79c6c4dfb6035ce68bad3088
ashen-develops <ashenp.designs@gmail.com>
2020-12-12 10:32:54 -0600

    updated app to use new error handler

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const foldersRouter = require('./folder/folder-router');
+const validateBearerToken = require('./validate-bearer-token');
+const foldersRouter = require('./folders/folders-router');
 const notesRouter = require('./notes/notes-router');
+const errorHandler = require('./error-handler');
 
 const app = express();
 
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
-app.use(morgan(morganSetting));
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test',
+  })
+);
 app.use(cors());
 app.use(helmet());
+app.use(validateBearerToken);
 
-app.use('/api/notes', notesRouter);
 app.use('/api/folders', foldersRouter);
+app.use('/api/notes', notesRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-// eslint-disable-next-line no-unused-vars
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    response = { error };
-  }
-  res.status(500).json(response);
-});
+app.use(errorHandler);
 
 module.exports = app;
